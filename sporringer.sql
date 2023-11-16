722d4cd7690c14f2c605aaa942d4d8e89441942c
sande <sander@skofsrud.net>
2023-11-16 12:54:07 +0100

    Updated queries

--- sporringer.sql
+++ sporringer.sql
@@ -1,6 +1,13 @@
 -- Velger alle bildene fra utstillingen med ID 1
-SELECT * FROM artwork
+SELECT 
+    artwork.Title, 
+    COALESCE(CONCAT(artist.FirstName, ' ', artist.LastName), artist.FirstName) AS ArtistName, 
+    artwork.Description, 
+    artwork.SaleStatus, 
+    exhibition.ExhibitionTitle 
+FROM artwork
 INNER JOIN exhibition ON artwork.Type = exhibition.TypeID
+JOIN artist ON artwork.ArtistID = artist.ArtistID
 WHERE exhibition.TypeID = 1;
 
 -- Velger alle bilder som er "Oil on canvas" og "Surrealistisk"
